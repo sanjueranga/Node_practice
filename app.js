e961d2454c08c89379c13f2cbb0f6ca8e2a6f0c7
sanjueranga <sanjueranga16@gmail.com>
2024-01-10 23:29:24 +0530

    WIP

--- app.js
+++ app.js
@@ -1,20 +1,24 @@
 const express = require("express");
+const app = express();
+
+
+
 const mongoose = require("mongoose");
 const path = require("path");
-const bodyParser = require("body-parser");
 const dotenv = require("dotenv");
-
 const envPath = path.resolve(__dirname, ".", ".env");
 dotenv.config({ path: envPath });
-
-const app = express();
-const PORT = process.env.PORT || 8080;
-
-app.use(bodyParser.json());
-
 const URL = process.env.MONGODB_URL;
-
 mongoose.connect(URL);
+const connection = mongoose.connection;
+connection.once("open", () => {
+  console.log("MongoDB connection success");
+});
+
+
+
+const bodyParser = require("body-parser");
+app.use(bodyParser.json());
 
 
 
@@ -22,12 +26,8 @@ mongoose.connect(URL);
 //Create A route to Create A user
 // /user
 
-const connection = mongoose.connection;
 
-connection.once("open", () => {
-  console.log("MongoDB connection success");
-});
-
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log("Server is up and running on : " + PORT);
 });
